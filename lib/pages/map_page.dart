d7ba6c71091d7719cf811a03c83c5068b2fe7c92
fen0268 <Kenshi150107@gmail.com>
2023-08-17 05:42:31 +0900

    refactor: tabbarview を追加、

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -14,83 +14,79 @@ class StartPage extends ConsumerWidget {
     final markers = ref.watch(markersStreamProvider);
 
     return DefaultTabController(
-      initialIndex: 0,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('ホーム'),
-          backgroundColor: Colors.black45,
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.sports_baseball)),
-              Tab(icon: Icon(Icons.sports_soccer)),
-              Tab(icon: Icon(Icons.sports_tennis)),
-            ],
-          ),
-        ),
-        body: Stack(
-          children: [
-            GoogleMap(
-              onMapCreated: (controller) {
-                mapController = controller;
-              },
-              zoomControlsEnabled: false,
-              initialCameraPosition: initialCameraPosition,
-              minMaxZoomPreference: const MinMaxZoomPreference(11, 20),
-              markers: markers.when(
-                data: (markerData) {
-                  return Set<Marker>.of(
-                    markerData.map(
-                      (stop) {
-                        return Marker(
-                          markerId: MarkerId(stop.id),
-                          position: LatLng(stop.stopLat, stop.stopLon),
-                          infoWindow: InfoWindow(
-                            title: stop.stopName,
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-                loading: () =>
-                    {}, // You can show a loading indicator here if needed
-                error: (_, __) =>
-                    {}, // You can handle error state here if needed
-              ),
-            ),
-            Container(
-              height: 80,
-              child: const TabBarView(
-                physics: NeverScrollableScrollPhysics(),
-                children: <Widget>[
-                  TextField(
-                    maxLength: 50,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                      ),
-                      fillColor: Colors.white54,
-                      filled: true,
-                      icon: Icon(Icons.search),
-                      hintText: "バス停の名前を入れてね",
-                    ),
-                  ),
-                  Text(
-                    'サッカー',
-                    style: TextStyle(fontSize: 32.0),
-                  ),
-                  Text(
-                    'テニス',
-                    style: TextStyle(fontSize: 32.0),
-                  ),
+      length: 2,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: AppBar(
+              backgroundColor: Colors.black45,
+              bottom: const TabBar(
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.search)),
+                  Tab(icon: Icon(Icons.map)),
                 ],
               ),
-            )
-          ],
+            ),
+          ),
+          body: Stack(
+            children: [
+              GoogleMap(
+                onMapCreated: (controller) {
+                  mapController = controller;
+                },
+                zoomControlsEnabled: false,
+                initialCameraPosition: initialCameraPosition,
+                minMaxZoomPreference: const MinMaxZoomPreference(11, 20),
+                markers: markers.when(
+                  data: (markerData) {
+                    return Set<Marker>.of(
+                      markerData.map(
+                        (stop) {
+                          return Marker(
+                            markerId: MarkerId(stop.id),
+                            position: LatLng(stop.stopLat, stop.stopLon),
+                            infoWindow: InfoWindow(
+                              title: stop.stopName,
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  loading: () =>
+                      {}, // You can show a loading indicator here if needed
+                  error: (_, __) =>
+                      {}, // You can handle error state here if needed
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+                child: TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 5, 0),
+                      child: TextField(
+                        maxLength: 50,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(),
+                          fillColor: Colors.white54,
+                          filled: true,
+                          icon: Icon(Icons.search),
+                          hintText: 'バス停の名前を入れてね',
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'サッカー',
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
