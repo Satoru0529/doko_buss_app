b7b3390cd88bec36678e6ce2f7c100183a5e7ada
fen0268 <Kenshi150107@gmail.com>
2023-08-17 00:08:07 +0900

    feat: すべてのモデルクラスの withonverter

--- lib/repository/firestore.dart
+++ lib/repository/firestore.dart
@@ -1,12 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../model/stops.dart';
+import '../model/stop_times/stop_times.dart';
+import '../model/stops/stops.dart';
+import '../model/trips/trips.dart';
 
 final stopsRef =
     FirebaseFirestore.instance.collection('stops').withConverter<Stops>(
           // Reference to the class
           /// decode
           fromFirestore: (snapshot, _) => Stops.fromJson(snapshot.data()!),
+
+          /// encode
+          toFirestore: (value, _) => value.toJson(),
+        );
+
+final stopTimesRef = FirebaseFirestore.instance
+    .collection('stop_times')
+    .withConverter<StopTimes>(
+      // Reference to the class
+      /// decode
+      fromFirestore: (snapshot, _) => StopTimes.fromJson(snapshot.data()!),
+
+      /// encode
+      toFirestore: (value, _) => value.toJson(),
+    );
+
+final tripsRef =
+    FirebaseFirestore.instance.collection('trips').withConverter<Trips>(
+          // Reference to the class
+          /// decode
+          fromFirestore: (snapshot, _) => Trips.fromJson(snapshot.data()!),
 
           /// encode
           toFirestore: (value, _) => value.toJson(),
