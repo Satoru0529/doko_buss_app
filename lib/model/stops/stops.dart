07d100957b06011182c2667b18f3026ef4a77b10
fen0268 <Kenshi150107@gmail.com>
2023-08-16 16:40:27 +0900

    feat:

--- lib/model/stops/stops.dart
+++ lib/model/stops/stops.dart
@@ -2,14 +2,24 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'stops.freezed.dart';
 part 'stops.g.dart';
+
 @freezed
 class Stops with _$Stops {
   const factory Stops({
+    /// Doc ID
     @Default('') String id,
-    @Default('') String stopId, // 1
-    @Default('') String stopName, // 3
-    @Default(0) double stopLat, // 5
-    @Default(0) double stopLon, // 6
+
+    /// num_1 or num_2 (上り or 下り)
+    @Default('') String stopId,
+
+    /// バス停名
+    @Default('') String stopName,
+
+    /// バス停の緯度
+    @Default(0) double stopLat,
+
+    /// バス停の経度
+    @Default(0) double stopLon,
   }) = _Stops;
 
   factory Stops.fromJson(Map<String, dynamic> json) => _$StopsFromJson(json);
