c69e42c0dcc15fbb868b9f8b7e2f8a7f78b183e7
fen0268 <Kenshi150107@gmail.com>
2023-08-16 16:44:38 +0900

    refactor: コメント追加、インデント

--- lib/provider/map_provider.dart
+++ lib/provider/map_provider.dart
@@ -4,28 +4,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../model/stops/stops.dart';
 
-final mapControllerProvider = Provider<GoogleMapController?>((ref) {
-  // GoogleMapControllerの初期化はStartPage内で行うため、ここではnullを返します
-  return null;
-});
+final mapControllerProvider = Provider<GoogleMapController?>(
+  (ref) {
+    // GoogleMapControllerの初期化はStartPage内で行うため、ここではnullを返します
+    return null;
+  },
+);
 
-final cameraPositionProvider = Provider<CameraPosition>((ref) {
-  return const CameraPosition(
-    target: LatLng(38.7812934, 140.2464705),
-    zoom: 13,
-  );
-});
+final cameraPositionProvider = Provider<CameraPosition>(
+  (ref) {
+    return const CameraPosition(
+      target: LatLng(38.7812934, 140.2464705),
+      zoom: 13,
+    );
+  },
+);
 
-final markersStreamProvider = StreamProvider<List<Stops>>((ref) {
-  final collection = FirebaseFirestore.instance.collection('stops');
-  final stream = collection.snapshots().map(
-        (snapshot) => snapshot.docs.map((doc) {
-          final data = doc.data();
-          return Stops.fromJson({
-            ...data,
-            'id': doc.id, // ドキュメントのIDを追加
-          });
-        }).toList(),
-      );
-  return stream;
-});
+final markersStreamProvider = StreamProvider<List<Stops>>(
+  (ref) {
+    /// stops コレクションのドキュメントを全て取得
+    final collection = FirebaseFirestore.instance.collection('stops');
+
+    /// CollectionReference 型で受け取った値を、Stream<List<Stops>> 型で返す
+    return collection.snapshots().map(
+          (snapshot) => snapshot.docs.map(
+            (doc) {
+              final data = doc.data();
+              return Stops.fromJson(
+                {
+                  ...data,
+                  'id': doc.id, // ドキュメントのIDを追加
+                },
+              );
+            },
+          ).toList(),
+        );
+  },
+);
