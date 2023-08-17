9eba27c289219b933a239e672f29b716daad00ef
fen0268 <Kenshi150107@gmail.com>
2023-08-17 20:05:48 +0900

    refactor: firestore の値を全部取ってきてしまうので一度アンコメント

--- lib/repository/firestore.dart
+++ lib/repository/firestore.dart
@@ -1,21 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../model/stop_times/stop_times.dart';
-import '../model/stops/stops.dart';
-import '../model/trips/trips.dart';
+// import '../model/stop_times/stop_times.dart';
+// import '../model/stops/stops.dart';
+// import '../model/trips/trips.dart';
 
-final stopsRef = FirebaseFirestore.instance.collection('stops').withConverter(
-      fromFirestore: (snapshot, _) => Stops.fromJson(snapshot.data()!),
-      toFirestore: (value, _) => value.toJson(),
-    );
+// final stopsRef = FirebaseFirestore.instance.collection('stops').withConverter(
+//       fromFirestore: (snapshot, _) => Stops.fromJson(snapshot.data()!),
+//       toFirestore: (value, _) => value.toJson(),
+//     );
 
-final stopTimesRef =
-    FirebaseFirestore.instance.collection('stop_times').withConverter(
-          fromFirestore: (snapshot, _) => StopTimes.fromJson(snapshot.data()!),
-          toFirestore: (value, _) => value.toJson(),
-        );
+// final stopTimesRef =
+//     FirebaseFirestore.instance.collection('stop_times').withConverter(
+//           fromFirestore: (snapshot, _) => StopTimes.fromJson(snapshot.data()!),
+//           toFirestore: (value, _) => value.toJson(),
+//         );
 
-final tripsRef = FirebaseFirestore.instance.collection('trips').withConverter(
-      fromFirestore: (snapshot, _) => Trips.fromJson(snapshot.data()!),
-      toFirestore: (value, _) => value.toJson(),
-    );
+// final tripsRef = FirebaseFirestore.instance.collection('trips').withConverter(
+//       fromFirestore: (snapshot, _) => Trips.fromJson(snapshot.data()!),
+//       toFirestore: (value, _) => value.toJson(),
+//     );
