cb9666313231e5eb2bdee90ae1c283d52c09896f
fen0268 <Kenshi150107@gmail.com>
2023-08-18 19:26:49 +0900

    feat: 検索機能を作成

--- lib/provider/search/search_provider.g.dart
+++ lib/provider/search/search_provider.g.dart
@@ -6,12 +6,12 @@ part of 'search_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$searchNotifierHash() => r'5e5cdb6a89fd43a1571a6d073a989ae099aa4184';
+String _$searchNotifierHash() => r'0e97e5492293d8ce87d37f9cc7aecf9e6e10ba85';
 
 /// See also [SearchNotifier].
 @ProviderFor(SearchNotifier)
 final searchNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<SearchNotifier, List<Stops>>.internal(
+    AutoDisposeAsyncNotifierProvider<SearchNotifier, List<StopLow>>.internal(
   SearchNotifier.new,
   name: r'searchNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final searchNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$SearchNotifier = AutoDisposeAsyncNotifier<List<Stops>>;
+typedef _$SearchNotifier = AutoDisposeAsyncNotifier<List<StopLow>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
