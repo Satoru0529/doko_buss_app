91c1e052731dfa7c0db8c5807fdacac53e8ace7e
fen0268 <Kenshi150107@gmail.com>
2023-08-21 01:23:25 +0900

    feat: ルート表示する provider を作成

---
+++ lib/provider/buss_route/buss_route_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'buss_route_provider.g.dart';
+
+@riverpod
+class BussRouteProvider extends _$BussRouteProvider {
+@override
+FutureOr<void> build() {}
+
+
+}
---
+++ lib/provider/buss_route/buss_route_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'buss_route_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bussRouteProviderHash() => r'f716a50eefe0aba617a4fd7f2f16ea15a353f521';
+
+/// See also [BussRouteProvider].
+@ProviderFor(BussRouteProvider)
+final bussRouteProviderProvider =
+    AutoDisposeAsyncNotifierProvider<BussRouteProvider, void>.internal(
+  BussRouteProvider.new,
+  name: r'bussRouteProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bussRouteProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BussRouteProvider = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
