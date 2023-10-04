5f103ab1169bc35389c88a7b99192ee00b68ff8a
fen0268 <Kenshi150107@gmail.com>
2023-10-04 22:56:04 +0900

    refactor: dark モードを削除

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'pages/map_page.dart';
-import 'provider/theme_mode_provider.dart';
+import 'pages/map/map_page.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeModeProvider.notifier);
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData.dark(),
-      themeMode: themeMode.state,
-      home: const MapPage(),
+      home: MapPage(),
     );
   }
 }
--- lib/provider/theme_mode_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// テーマモードの状態
-final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.system);
