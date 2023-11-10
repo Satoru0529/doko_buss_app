e7900d76621afb1e2695d87e235fa3ebc16f3fb4
fen0268 <Kenshi150107@gmail.com>
2023-11-11 04:11:59 +0900

    refactor: 型不一致を解消

--- lib/model/stop_times/stop_times.dart
+++ lib/model/stop_times/stop_times.dart
@@ -8,7 +8,7 @@ class StopTimes with _$StopTimes {
   const factory StopTimes({
     @Default('') String id,
     @Default('') String tripId,
-    @Default(0) String arrivalTime,
+    @Default('') String arrivalTime,
     @Default('') String stopId,
     @Default(0) int stopSequence,
     @Default('') String stopHeadsign,
