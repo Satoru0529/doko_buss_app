1aae5185fe717b29353668b3a601b0fade7534db
fen0268 <Kenshi150107@gmail.com>
2023-11-12 14:58:39 +0900

    feat: supabase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:buss_app/util/api_key.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Supabase 初期化
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
 
   /// 位置情報サービスが有効かの確認
   await Future(() async {
--- lib/util/api_key.dart
+++ lib/util/api_key.dart
@@ -1,3 +1,9 @@
 /// TODO gitignoreで管理
-/// todo Google Maps Platform API Key 
+/// todo Google Maps Platform API Key
 const String apiKey = 'AIzaSyCsi4yLKlnTJb74RpUMfDiXrgwfa_gvsXI';
+
+const String supabaseUrl = 'https://elkgsszfodvnacwjizkt.supabase.co';
+
+const String supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsa2dzc3pmb2R2bmFjd2ppemt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk2NjQzMjUsImV4cCI6MjAxNTI0MDMyNX0.80v2uM4AI1rxz2UwrLgC7ZT4W8KvauKHsHnsxDaWmhI';
+
